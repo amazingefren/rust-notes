1f0aae449d043f1bb03178629860dd25fe3c712e
amazingefren <efrentheroker@hotmail.com>
2020-09-28 18:42:01 -0600

    comments added, will come back and update for changes

---
+++ concepts/comments/src/main.rs
@@ -0,0 +1,8 @@
+// Quick and Easy, this is a comment
+/*
+    This is a documentation comment
+*/
+fn main() {
+    println!("Hello, world!"); // this can go here
+    println!("Hello, world!"); /* this can go here too */
+}
