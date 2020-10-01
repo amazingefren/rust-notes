403cb9307291288577de189b8b2e39e3886adc11
amazingefren <efrentheroker@hotmail.com>
2020-10-01 04:10:07 -0600

    explicit lifetime

--- concepts/traits/src/main.rs
+++ concepts/traits/src/main.rs
@@ -45,6 +45,30 @@ impl Summary for Tweet {
         format!("@{}", self.username)
     }
 }
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
 fn main() {
     let tweet = Tweet {
         username: String::from("amazingefren"),
@@ -53,6 +77,44 @@ fn main() {
         retweet: false,
     };
     println!("1 new tweet: {}", tweet.default_summarize());
-    println!("1 new tweet: {}", tweet.default_summarize());
     println!("1 new tweet: {}", tweet.read());
+
+    let pair = Pair::new(5, 10);
+    pair.cmp_display();
+    {
+        let r;
+        {
+            let x = 5;
+            r = x
+            // Here i am giving ownership of x to r
+            // &x is not used here as
+        } // x will be invalidated here, assigning a ptr to a ptr that doesn't exist doesn't work
+        println!("r: {}", r);
+    }
+    {
+        let x = 5;
+        let r = &x; // here there are to variables in the stack, the stack for x is never invalidated.
+                    // so assigning the ptr or 'r' to ptr of 'x' will work as it never gets invalidated
+        println!("r: {}", r); // so here to we can ref r, which points to ptr of 'x' pointing to heap of x being 5
+        let word1 = "Hello";
+        let word2 = "World!";
+        let longest_word = longest(word1, word2);
+        println!("Longest Word: {}", longest_word);
+    }
+}
+
+// Lifetime annotations in Function
+/*
+    example types:
+    &i32 - explicit
+    &'a i32 - explicit lifetime
+    &'a mut i32 - mutable explicit lifetime
+    ( 'a ) is setting a explicit lifetime
+*/
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
