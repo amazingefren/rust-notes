821da6a619938a5646a19860dc14f515accb9fd9
amazingefren <efrentheroker@hotmail.com>
2020-10-02 15:42:28 -0600

    iterators

---
+++ concepts/iterators/src/main.rs
@@ -0,0 +1,123 @@
+fn main() {
+    vec_iter();
+}
+
+fn vec_iter() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    for val in &v1 {
+        println!("v1 ---- Got: {}", val);
+    }
+    for val in v1_iter {
+        println!("v1_iter ---- Got: {}", val);
+    }
+    let v3: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    println!("v3: {:?}", v3);
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+struct Counter {
+    count: u8,
+}
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+impl Iterator for Counter {
+    type Item = u8;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    pub trait Iterator {
+        type Item;
+        fn next(&mut self) -> Option<Self::Item>;
+    }
+    #[test]
+    fn iter_demon() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+    #[test]
+    fn iter_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
+    use super::shoes_in_my_size;
+    use super::Shoe;
+    #[test]
+    fn filter_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+        let in_my_size = shoes_in_my_size(shoes, 10);
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                }
+            ]
+        )
+    }
+    use super::Counter;
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u8 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(18, sum);
+    }
+}
