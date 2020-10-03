f8990fbbc862195a9f73ff751ca5e87eb817ebc2
amazingefren <efrentheroker@hotmail.com>
2020-10-02 18:05:01 -0600

    refcell and interior mutability

--- concepts/pointers/src/main.rs
+++ concepts/pointers/src/main.rs
@@ -5,30 +5,53 @@
             * When size of value can't be known at compile time
             * Large amount of data and want to transfer ownership to prevent duplicating/copying data
 */
+#[derive(Debug)]
+enum Lists {
+    Cons(Rc<RefCell<i32>>, Rc<Lists>),
+    Nil,
+}
 
-// RefCell RC<T>  is like a living room, theres a tv and people watching it, when the first person comes in, its on. when the last person leaves, it turns off.
+use crate::Lists::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
+
+// RC<T>  is like a living room, theres a tv and people watching it, when the first person comes in, its on. when the last person leaves, it turns off.
 enum List {
     Cons(i32, Rc<List>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
+// use crate::List::{Cons, Nil};
 
 #[allow(unused)]
-fn main() {
+fn _rc_main() {
     // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
     // let b = Cons(3, Box::new(a)); // a is moved into b, b owns a.
     // let c = Cons(4, Box::new(a)); // by now, a has been moved
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("Count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("Count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("Count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("Count after C goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("Count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("Count after creating b = {}", Rc::strong_count(&a));
+    // {
+    // let c = Cons(4, Rc::clone(&a));
+    // println!("Count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("Count after C goes out of scope = {}", Rc::strong_count(&a));
 }
 
 #[allow(unused)]
